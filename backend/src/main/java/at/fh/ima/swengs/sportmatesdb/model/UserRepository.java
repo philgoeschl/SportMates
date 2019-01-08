d41b1b165e9fb7917d70d1469e05ff023701321d
Lukas <lukas.david@edu.fh-joanneum.at>
2019-01-08 11:11:19 +0100

    UserReporistory fixed

--- backend/src/main/java/at/fh/ima/swengs/sportmatesdb/model/UserRepository.java
+++ backend/src/main/java/at/fh/ima/swengs/sportmatesdb/model/UserRepository.java
@@ -10,23 +10,12 @@ import java.util.List;
 import java.util.Optional;
 
 @RepositoryRestResource
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-public interface UserRepository extends PagingAndSortingRepository<User,String> {
-=======
+
 public interface UserRepository extends PagingAndSortingRepository<User, String>, JpaRepository<User, String>, CrudRepository<User, String> {
->>>>>>> Stashed changes
 
 
     public List<User> findAll();
 
-<<<<<<< Updated upstream
-    public Optional<User> findByUsername (String userName);
-=======
-public interface UserRepository extends PagingAndSortingRepository<Sport,Long> {
-    User findByUsername(String username);
->>>>>>> Stashed changes
-=======
     public Optional<User> findByUsername(@Param("username") String username);
->>>>>>> Stashed changes
+
 }
