1ff7dddfd361f808b277a998e46fd6038df4e7d2
Lukas <lukas.david@edu.fh-joanneum.at>
2019-01-08 11:07:45 +0100

    Auto stash before merge of "master" and "origin/master"

--- backend/src/main/java/at/fh/ima/swengs/sportmatesdb/model/UserRepository.java
+++ backend/src/main/java/at/fh/ima/swengs/sportmatesdb/model/UserRepository.java
@@ -1,5 +1,7 @@
 package at.fh.ima.swengs.sportmatesdb.model;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,14 +11,22 @@ import java.util.Optional;
 
 @RepositoryRestResource
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 public interface UserRepository extends PagingAndSortingRepository<User,String> {
+=======
+public interface UserRepository extends PagingAndSortingRepository<User, String>, JpaRepository<User, String>, CrudRepository<User, String> {
+>>>>>>> Stashed changes
 
 
     public List<User> findAll();
 
+<<<<<<< Updated upstream
     public Optional<User> findByUsername (String userName);
 =======
 public interface UserRepository extends PagingAndSortingRepository<Sport,Long> {
     User findByUsername(String username);
+>>>>>>> Stashed changes
+=======
+    public Optional<User> findByUsername(@Param("username") String username);
 >>>>>>> Stashed changes
 }
